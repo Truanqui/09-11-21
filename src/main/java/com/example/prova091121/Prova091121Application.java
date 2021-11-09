073211e2d1eb5664848cc6e2703f16219d5c2ce0
Johann <johannteixeira2@gmail.com>
2021-11-09 20:27:39 -0300

    Initial commit

---
+++ src/main/java/com/example/prova091121/Prova091121Application.java
@@ -0,0 +1,13 @@
+package com.example.prova091121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova091121Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prova091121Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prova091121/Prova091121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prova091121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova091121ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
